47816737b7d003053574c7917bf389d78ad08140
gunasowndari <46969105+gunasowndari@users.noreply.github.com>
2026-09-19 16:58:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("Message Processor Application")
+import streamlit as st
+
+st.title("Message Processor")
+
+message = st.text_input("Enter your message")
+
+if st.button("Process Message"):
+    st.write("You entered:")
+    st.write(message)
