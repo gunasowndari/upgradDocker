56ac22d7c406a66d3323aa6e54f64c6d7127535e
gunasowndari <46969105+gunasowndari@users.noreply.github.com>
2026-09-19 17:46:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 import streamlit as st
+import requests
 
 st.title("Message Processor")
 
 message = st.text_input("Enter your message")
 
 if st.button("Process Message"):
-    st.write("You entered:")
-    st.write(message)
+
+    response = requests.post(
+        "http://localhost:8000/process",
+        params={"message": message}
+    )
+
+    result = response.json()
+
+    st.subheader("Result")
+    st.write(result["message"])
+    st.write(f"Characters: {result['length']}")
