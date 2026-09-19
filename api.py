a8b082ae38a9360468c2354d8ad2a03bb667e1c2
gunasowndari <46969105+gunasowndari@users.noreply.github.com>
2026-09-19 17:36:06 +0530

    Create api.py

---
+++ api.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.post("/process")
+def process_message(message: str):
+    return {
+        "message": message.upper(),
+        "length": len(message)
+    }
